065688c81102340871e84834515244ea51a33857
JDavis01 <jacobkdavis91@gmail.com>
2022-11-11 09:59:51 -0500

    initial project structure

---
+++ Backend/src/main/java/MysteryEducatorApp/MysteryEducatorAppApplication.java
@@ -0,0 +1,13 @@
+package MysteryEducatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteryEducatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysteryEducatorAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/MysteryEducatorApp/MysteryEducatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MysteryEducatorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysteryEducatorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
