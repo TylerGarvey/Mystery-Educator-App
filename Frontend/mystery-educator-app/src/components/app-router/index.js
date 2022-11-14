00ba875ef7d4dedd68c344ca83fe541982609e37
JDavis01 <jacobkdavis91@gmail.com>
2022-11-13 20:43:16 -0500

    fixed bug and added routing

---
+++ Frontend/mystery-educator-app/src/components/app-router/index.js
@@ -0,0 +1,13 @@
+import { Route, Switch } from "react-router-dom";
+import DinoQuiz from "../../pages/DinoQuiz";
+
+
+const PageSwitch = () => {
+    return (
+        <Switch>
+            <Route exact path={'/dinoQuiz'} component={DinoQuiz} />
+        </Switch>
+    )
+}
+
+export default PageSwitch
---
+++ Frontend/mystery-educator-app/src/pages/DinoQuiz.js
@@ -0,0 +1,6 @@
+
+const DinoQuiz = () => (
+    <h1>Dino Quiz!</h1>
+)
+
+export default DinoQuiz
