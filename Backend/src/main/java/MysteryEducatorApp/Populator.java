be5510ece9cba4fd5cd158d2fad5d40a470a8ab2
Ayanle6 <ayanle923@gmail.com>
2022-11-14 10:10:45 -0500

    .

--- Backend/src/main/java/MysteryEducatorApp/Populator.java
+++ Backend/src/main/java/MysteryEducatorApp/Populator.java
@@ -1,4 +1,26 @@
 package MysteryEducatorApp;
 
-public class Populator {
+@Component
+public class Populator implements CommandLineRunner{
+
+    private DinosaurRepo DinoRepo;
+
+    public Populator(DinosaurRepo DinoRepo) {
+        this.DinoRepo = DinoRepo;
+    }
+
+    @Override
+    public void run(string... args) throws Exception {
+
+        Dinosaur Dinosaur1 = new Dinosaur();
+        DinoRepo.save(Dinosaur)
+
+
+        Dinosaur Dinosaur2 = new Dinosaur();
+        dinoRepo.save
+
+
+        Dinosaur Dinosaur3 = new Dinosaur();
+        dinoRepo.save
+
 }
