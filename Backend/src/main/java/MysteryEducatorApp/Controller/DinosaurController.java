c49e1bdccfc32c38b4f88896bb5850e55e900fe5
zekariye2246 <zekariye2246@gmail.com>
2022-11-15 13:27:41 -0500

    Controller progress

--- Backend/src/main/java/MysteryEducatorApp/Controller/DinosaurController.java
+++ Backend/src/main/java/MysteryEducatorApp/Controller/DinosaurController.java
@@ -1,2 +1,15 @@
-package MysteryEducatorApp.Controller;public class DinosaurController {
+package MysteryEducatorApp.Controller;
+
+import MysteryEducatorApp.Repo.DinosaurRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DinosaurController {
+    private DinosaurRepository DinoRepo;
+
+    public DinosaurController(DinosaurRepository dinoRepo) {
+        DinoRepo = dinoRepo;
+    }
+
+    
 }
