528fc426db26e19be3308e92d1061889417282f7
TylerGarvey <redgarvey@gmail.com>
2022-11-16 11:14:13 -0500

    BoredPage is functional

--- Frontend/mystery-educator-app/src/pages/BoredPage.js
+++ Frontend/mystery-educator-app/src/pages/BoredPage.js
@@ -4,57 +4,45 @@ import React, { useEffect, useState } from 'react';
 const BoredPage =() => {
  
     const [ loading, setLoading ] = useState(true);
-    const [ activity, setActivity ] = useState([]);
+    const [ activities, setActivities ] = useState([]);
  
    function loadActivity() {
         setLoading(true);
         fetch(`http://www.boredapi.com/api/activity/`)
-            .then(response => response.data())
-            .then(json => {
-                setActivity([...activity, data.activity]);
+            .then(response => response.json())
+            .then(data => {
+                setActivities([...activities, data]);
                 setLoading(false);
             });
     }
        
     useEffect(() => {loadActivity();}, []);
         
-    if(loading)  {
-        return <p>Loading...how boring</p>
+    if(activities.length === 0)  {
+        return <p>Loading...how boring...</p>
     }
     
     return(
        
-                ( 
+                 
                     <div className="boredApiContainer">
                         <div>
                             <div className='title'>
                                 <h1>The Bored API: Let's find you something to do</h1>
-                                <h2>{activity}</h2>
+                                <ul>
+                                    {activities.map((activity) => {
+                                        return<li key={activity.key}>{activity.activity}</li>
+                                    })}
+                                </ul>
                             </div>
-                            <button onClick={loadActivity} type="text" class="boredButton">Click me if you're bored!</button>
+                            <button disabled={loading} onClick={loadActivity} type="text" class="boredButton">Click me if you're bored!</button>
                         </div>
                     </div>
                   
-                )
+                
             )
         
     
-
-        // <div className="boredContainer">
-        //     <div className="boredBodyContainer">
-        //         <p className="aboutBored">
-        //         The Bored API
-        //     Let's find you something to do
-        //         </p>
-        //     </div>
-        //     <div className="boredButtonContainer">
-        //         <div className="boredButton">
-        //             
-        //         </div>
-        //     </div>
-            
-    
-   
 }
     
  
