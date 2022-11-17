b662fb9443f71c5f96afe1373ba24c71569a91ad
Ayanle6 <ayanle923@gmail.com>
2022-11-17 12:12:30 -0500

    progress made on nasa.js

--- Frontend/mystery-educator-app/src/pages/Nasa.js
+++ Frontend/mystery-educator-app/src/pages/Nasa.js
@@ -1,3 +1,45 @@
-import React from "react";
+import React, { useEffect, useState} from "react";
+import { useParams } from "react-router-dom";
+
+const Nasa =() => {
+    
+const params = useParams();
+const [images, setImages] = useState([]);
+const [ loading, setLoading] = useState(true);
+
+
+useEffect(() => {
+    fetch('')
+})
+
+return (
+
+)
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Nasa;
